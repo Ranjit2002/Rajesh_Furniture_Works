c89d5d1ddf746b920835f7b65e59c7b2500868c2
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2026-09-08 11:01:09 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Rajesh_Furniture_Works',
   reactStrictMode: true,
   devIndicators: false,
   images: {
